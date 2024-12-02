ed30076d7716a8c3a91f3dccf6851ec15f075be2
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-12-02 17:02:55 -0500

    added personal data and photos model

---
+++ API/Data/Migrations/20241202220158_addData.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FavoriteTCG",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Handle",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreferredFormat",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    PublicId = table.Column<string>(type: "TEXT", nullable: true),
+                    AppUserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AppUserId",
+                table: "AspNetUsers",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photos",
+                column: "AppUserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AspNetUsers_AppUserId",
+                table: "AspNetUsers",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AspNetUsers_AppUserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AppUserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FavoriteTCG",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Handle",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredFormat",
+                table: "AspNetUsers");
+        }
+    }
+}
--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -7,12 +7,12 @@ namespace API.Models;
 public class AppUser : IdentityUser<int>
 {
     public ICollection<AppUserRole> UserRoles { get; set; } = [];
-    // public string? Location { get; set; } // TODO: this will need a useful implementation. This is only a placeholder for now
+    public string? Location { get; set; } // TODO: this will need a useful implementation. This is only a placeholder for now
 
-    // public string? Handle { get; set; }
-    // public string? FavoriteTCG { get; set; }
-    // public string? About { get; set; }
-    // public Photo? ProfilePicture { get; set; }
-    // public string? PreferredFormat { get; set; }
-    // public ICollection<AppUser>? Friends { get; set; }
+    public string? Handle { get; set; }
+    public string? FavoriteTCG { get; set; }
+    public string? About { get; set; }
+    public Photo? ProfilePicture { get; set; }
+    public string? PreferredFormat { get; set; }
+    public ICollection<AppUser>? Friends { get; set; }
 }
