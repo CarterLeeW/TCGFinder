9818f0aaa2b6549a8afe762ebacfcf82c4628134
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-12-02 16:41:11 -0500

    problems with identity

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace API.Models;
@@ -6,4 +7,12 @@ namespace API.Models;
 public class AppUser : IdentityUser<int>
 {
     public ICollection<AppUserRole> UserRoles { get; set; } = [];
+    // public string? Location { get; set; } // TODO: this will need a useful implementation. This is only a placeholder for now
+
+    // public string? Handle { get; set; }
+    // public string? FavoriteTCG { get; set; }
+    // public string? About { get; set; }
+    // public Photo? ProfilePicture { get; set; }
+    // public string? PreferredFormat { get; set; }
+    // public ICollection<AppUser>? Friends { get; set; }
 }
---
+++ API/Models/Photo.cs
@@ -0,0 +1,15 @@
+// using System;
+// using System.ComponentModel.DataAnnotations.Schema;
+
+// namespace API.Models;
+
+// [Table("Photos")]
+// public class Photo
+// {
+//     public int Id { get; set; }
+//     public string? Url { get; set; }
+//     public string? PublicId { get; set; }
+//     // Navigation properties - required one-to-one relationship
+//     public int? AppUserId { get; set; }
+//     public AppUser? AppUser { get; set; } = null!;
+// }
